7e64d5f3f8f0088c24d8c526a08c72bae5f20080
Elena Padun <whiteangel64rus@gmail.com>
2023-12-05 01:52:20 +0300

    feat: api requests have been created

---
+++ src/utils/Api.js
@@ -0,0 +1,97 @@
+const MAIN_API = "http://81.31.246.159/api/v1";
+
+class Api {
+  constructor ({ address }) {
+    this.address = address;
+  }
+
+  _checkResponse (res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(new Error(`Ошибка ${res.status}`));
+  }
+
+  postMatches ({ product_id, dealer_id, key }) {
+    return fetch(`${this.address}/matches`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ product_id, dealer_id, key })
+    })
+      .then(this._checkResponse);
+  }
+
+  putMatches ({ product_id, dealer_id, key, id }) {
+    return fetch(`${this.address}/matches/${id}`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ product_id, dealer_id, key, id })
+    })
+      .then(this._checkResponse);
+  }
+
+  putchMatches ({ product_id, dealer_id, key, id }) {
+    return fetch(`${this.address}/matches/${id}`, {
+      method: "PUTCH",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ product_id, dealer_id, key, id })
+    })
+      .then(this._checkResponse);
+  }
+
+  deleteMatches ({ id }) {
+    return fetch(`${this.address}/matches/${id}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(this._checkResponse);
+  }
+
+  getOwnProducts () {
+    return fetch(`${this.address}/own-products`, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(this._checkResponse);
+  }
+
+  getProducts () {
+    return fetch(`${this.address}/products`, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(this._checkResponse);
+  }
+
+  getProductsId ({ id }) {
+    return fetch(`${this.address}/products/${id}`, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(this._checkResponse);
+  }
+
+  deleteProductsId ({ id }) {
+    return fetch(`${this.address}/products/${id}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(this._checkResponse);
+  }
+}
+const MainApi = new Api({
+  address: MAIN_API
+});
+export default MainApi;
